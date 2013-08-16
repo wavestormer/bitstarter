dd572bd9cc81a6b496e4fc5733108ece002ec889
Ubuntu <ubuntu@ip-10-178-24-50.ec2.internal>
2013-08-16 03:03:11 +0000

    commit for deploy to heroku

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send("Hello World 2!");
 });
 
 var port = process.env.PORT || 5000;
