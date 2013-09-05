8a9787a3a1f78284aebb065c7aeadd4e693c0702
Ubuntu <ubuntu@ip-10-164-56-66.ec2.internal>
2013-09-05 23:45:50 +0000

    Made it the correct answer

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
 var express = require('express');
-var app = express();
 var fs = require('fs');
+var htmlfile = "index.html";
+
+var app = express.createServer(express.logger());
 
 
-app.use(express.logger());
-
-var data = fs.readFileSync('index.html', 'utf8');
 
 app.get('/', function(request, response) {
-  response.send(data);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
